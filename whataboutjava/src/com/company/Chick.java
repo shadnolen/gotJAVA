9cbc921f727f24e4a830991b4fc232c722be7a42
Shad Nolen <shadnolen@msn.com>
2018-12-03 20:24:13 -0800

    main
    Java

--- whataboutjava/src/com/company/Chick.java
+++ whataboutjava/src/com/company/Chick.java
@@ -7,7 +7,16 @@ public class Chick {
     public Chick() {
         name = "Tiny";
         System.out.println("setting constructor");
+
     }
     public static void main(String[] args) {
         Chick chick = new Chick();
-        System.out.println(chick.name); } }
+        System.out.println(chick.name);
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(56);
+        System.out.println(3);
+        System.out.println(017);
+        System.out.println(0x1F);
+    }
+}

f19c44fc09fa2f428421d41adfd409980077bfb8
Shad Nolen <shadnolen@msn.com>
2018-12-04 18:37:30 -0800

    javaSwitch
    testing data on the swtich

--- whataboutjava/src/com/company/Chick.java
+++ whataboutjava/src/com/company/Chick.java
@@ -22,13 +22,24 @@ public class Chick {
     int daysOfTheWeek = 6;
     switch(daysOfTheWeek){
         default:
-            System.out.println("weekday");
+            System.out.println("Used to be rock, but now it's roll\n" +
+                    "Looking at the clock and the remote control\n" +
+                    "The circle contracts till you all alone");
             break;
         case 0:
-            System.out.println("suday fun day");
+            System.out.println("I put that on the people you've outgrown\n" +
+                    "And maybe it was all for show\n" +
+                    "But it was so convincing that the ground got cold");
             break;
         case 6:
-            System.out.println("Saturday shun");
+            System.out.println(" love you like a brother\n" +
+                    "Even though I'm not your brother\n" +
+                    "We all got a few flaws\n" +
+                    "But let's try not to get too lost in the applause\n" +
+                    "I love you like a sister\n" +
+                    "Even though you're not my sister\n" +
+                    "We all got a few flaws\n" +
+                    "But let's try not to get too lost");
             break;
     }
     }

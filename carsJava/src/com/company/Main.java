e2b05cab8ff8dfa3897bff5ff4b13d7fa79b4f4b
Shad Nolen <shadnolen@msn.com>
2018-12-06 13:39:38 -0800

    myCarCode
    creating a car picker app

--- carsJava/src/com/company/Main.java
+++ carsJava/src/com/company/Main.java
@@ -8,6 +8,14 @@ public class Main {
 	Car myCar = new Car(32.2,"4dubs",Color.cyan,false);
 	Car nextCar = new Car(25.5,"iyesca",Color.black,true);
 	System.out.println("Plate One: " + myCar.licensePlate);
-	System.out.println("Plate One: " + nextCar.licensePlate);
+
+        System.out.println("TailLights working car One?: " + myCar.taillightWorking);
+        System.out.println("Plate One: " + myCar.paintColor);
+        System.out.println("MPG  Car One: " + myCar.averageMPG);
+        System.out.println("Plate Two: " + nextCar.licensePlate);
+        System.out.println("TailLights working car Two?: " + nextCar.taillightWorking);
+        System.out.println("Plate Two: " + nextCar.paintColor);
+        System.out.println("MPG Car Two: " + nextCar.averageMPG);
+
     }
 }

0bfcc8f1345a0a1366946064a47bb5372d995e95
Shad Nolen <shadnolen@msn.com>
2018-12-06 13:08:20 -0800

    CreatingCar

---
+++ carsJava/src/com/company/Car.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.awt.*;
+
+public class Car {
+    //datatypes
+    int averageMPG;
+    String licensePlate;
+    Color paintColor;
+    Boolean taillightWorking;
+
+    public Car(int inputaverageMPG, String inputLicensePlate, Color inputPaintColor, Boolean inputTaillightWorking ){
+
+    }
+
+}

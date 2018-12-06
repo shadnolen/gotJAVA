4d2b6bcf07037cab90aaac0667ded647a0b8f2c2
Shad Nolen <shadnolen@msn.com>
2018-12-06 13:33:14 -0800

    pullingData
    pulling data from classes

--- carsJava/src/com/company/Car.java
+++ carsJava/src/com/company/Car.java
@@ -4,13 +4,17 @@ import java.awt.*;
 
 public class Car {
     //datatypes
-    int averageMPG;
+    double averageMPG;
     String licensePlate;
     Color paintColor;
     Boolean taillightWorking;
 
-    public Car(int inputaverageMPG, String inputLicensePlate, Color inputPaintColor, Boolean inputTaillightWorking ){
+
+    public Car(double inputAverageMPG, String inputLicensePlate, Color inputPaintColor, Boolean inputTaillightWorking ){
+        this.averageMPG = inputAverageMPG;
+        this.licensePlate = inputLicensePlate;
+        this.paintColor = inputPaintColor;
+        this.taillightWorking = inputTaillightWorking;
 
     }
-
 }
--- carsJava/src/com/company/Main.java
+++ carsJava/src/com/company/Main.java
@@ -1,8 +1,13 @@
 package com.company;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Car myCar = new Car(32.2,"4dubs",Color.cyan,false);
+	Car nextCar = new Car(25.5,"iyesca",Color.black,true);
+	System.out.println("Plate One: " + myCar.licensePlate);
+	System.out.println("Plate One: " + nextCar.licensePlate);
     }
 }

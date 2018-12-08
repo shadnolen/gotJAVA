bac4b8b11965990fd8b4e2d38845e09b342d31a8
Shad Nolen <shadnolen@msn.com>
2018-12-07 22:36:45 -0800

    addingINTS

---
+++ carsJava/src/com/company/AddingInts.java
@@ -0,0 +1,29 @@
+package com.company;
+import java.util.Scanner;
+
+    public class AddingInts {
+
+    // adding int
+    int number1;
+    int number2;
+    int number3;
+    int number4;
+    int sum;
+
+    // using user input Scanner class Yo
+        System.out.println("enter First integer");
+    number1 = keyboard.nextInt();
+
+        System.out.println("enter Second integer");
+    number2 = keyboard.nextInt();
+
+        System.out.println("enter Third integer");
+    number3 = keyboard.nextInt();
+
+        System.out.println("enter Fourth integer");
+    number4 = keyboard.nextInt();
+
+    sum = number1 * number2 / number3 % number4;
+        System.out.println("Here is your sum of it all: " + sum);
+
+}
--- carsJava/src/com/company/Main.java
+++ carsJava/src/com/company/Main.java
@@ -1,12 +1,14 @@
 package com.company;
 
 import java.awt.*;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        //methods man
 	Car myCar = new Car(32.2,"4dubs",Color.cyan,false);
 	Car nextCar = new Car(25.5,"iyesca",Color.black,true);
+	//stringbuilder in effect
 	String string1 = "I love you like a brother\n" +
             "Even though I'm not your brother\n" +
             "We all got a few flaws\n" +
@@ -15,7 +17,11 @@ public class Main {
             "Even though you're not my sister\n" +
             "We all got a few flaws\n" +
             "But let's try not to get too lost";
-	    System.out.println("Plate One: " + myCar.licensePlate);
+Scanner keyboard = new Scanner(System.in);
+
+
+        // print that shhhhhh
+	    /*System.out.println("Plate One: " + myCar.licensePlate);
         System.out.println("TailLights working car One?: " + myCar.taillightWorking);
         System.out.println("Plate One: " + myCar.paintColor);
         System.out.println("MPG  Car One: " + myCar.averageMPG);
@@ -23,7 +29,7 @@ public class Main {
         System.out.println("TailLights working car Two?: " + nextCar.taillightWorking);
         System.out.println("Plate Two: " + nextCar.paintColor);
         System.out.println("MPG Car Two: " + nextCar.averageMPG);
-        System.out.println(new StringBuilder().append(string1).append(" ").append(string2).toString());
+        System.out.println(new StringBuilder().append(string1).append(" ").append(string2).toString());*/
 
 
 

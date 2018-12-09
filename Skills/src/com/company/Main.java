bb45817c6503b98e271a78cf5bb043729a899dcf
Shad Nolen <shadnolen@msn.com>
2018-12-09 13:39:00 -0800

    StringBuilderClasses
    working with string builders

---
+++ Skills/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+	//StringBuilder a = new StringBuilder("Air D&D");
+	//StringBuilder b = new StringBuilder("Rob Sonic");
+	StringBuilder sb = new StringBuilder ("Defriender");
+	String s1 = "Of Da";
+	String s2 = "Faith";
+	String s3 = "Streets";
+	//String sub = sb.substring(sb.indexOf("e"), sb.indexOf("ed"));
+	//b = b.append(" Da O.").append("G.");
+
+//	int len = sb.length();
+//	char ch = sb.charAt(6);
+
+	System.out.println(s1 + " " + s2 + " " + s1 + " " + s3);
+	System.out.println(sb);
+
+	//System.out.println("a= " + a);
+	//System.out.println("b= " + b);
+    }
+}

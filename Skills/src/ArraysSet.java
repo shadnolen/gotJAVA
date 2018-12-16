eb6ed03d4b939bc3f12fcb140694e9841d1cd53f
Shad Nolen <shadnolen@msn.com>
2018-12-15 21:50:15 -0800

    Array2HashSet
    Right here we're going to convert an array to Hash set

--- Skills/src/ArraysSet.java
+++ Skills/src/ArraysSet.java
@@ -8,12 +8,13 @@ public class ArraysSet {
     public static void main(String[] args){
         String[] array ={"We", "Are", "The", "World", "One", "Two", "Three"};
         Set<String> set = new HashSet<>(Arrays.asList(array));
-        System.out.println("Set: " + set);
+        System.out.println("Set1: " + set);
 
 
             String[] array1 = {"a","b","c"};
             Set<String> set1 = new HashSet<>(Arrays.asList(array1));
-            System.out.println("Set " + set1);
+            System.out.println("Set2: " + set1);
     }
+
 
 }

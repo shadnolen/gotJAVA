30fe67e7a7f9b2fdd28bac996a8b91edadcab8b5
Shad Nolen <shadnolen@msn.com>
2018-12-21 15:58:46 -0800

    ControllerActions
    working on adding controllers

--- InventorySystem/src/Controllers/imsController.java
+++ InventorySystem/src/Controllers/imsController.java
@@ -5,12 +5,18 @@
  */
 package Controllers;
 
+import Code.Parts;
+import Code.Products;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
 
 /**
  *
@@ -22,10 +28,20 @@ public class imsController  implements Initializable {
     private Label label;
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
+    private TextField partSearchField;
+    
+    @FXML
+    private TextField proSearchField;
+    
+    @FXML
+    public TableView<Parts>  PartsTable;
+    
+    @FXML
+    private TableView<Products> productsTable;
+
+    private ObservableList<Parts> partInventory = FXCollections.observableArrayList();
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {

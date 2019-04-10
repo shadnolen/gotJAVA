981c13ee833e102577f1a8d30e61b815d93e0e93
Shad Nolen <shadnolen@msn.com>
2019-04-10 13:16:56 -0700

    Update MainScreenController.java

--- InvSupply/src/Controllers/MainScreenController.java
+++ InvSupply/src/Controllers/MainScreenController.java
@@ -5,14 +5,22 @@
  */
 package Controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -59,11 +67,18 @@ public class MainScreenController implements Initializable {
     }
 
     @FXML
-    private void addButton(ActionEvent event) {
+    private void addButton(ActionEvent event) throws IOException {
+        Parent parentPage = FXMLLoader.load(getClass().getResource("/Views/PartUpdate.fxml"));
+        Scene scene = new Scene(parentPage);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();       
     }
 
     @FXML
     private void ModifyButton(ActionEvent event) {
+        
+        
     }
 
     @FXML
@@ -75,7 +90,12 @@ public class MainScreenController implements Initializable {
     }
 
     @FXML
-    private void prouctAddButton(ActionEvent event) {
+    private void prouctAddButton(ActionEvent event) throws IOException {
+        Parent parentPage = FXMLLoader.load(getClass().getResource("/Views/ProductUpdate.fxml"));
+        Scene scene = new Scene(parentPage);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();      
     }
 
     @FXML
@@ -88,6 +108,10 @@ public class MainScreenController implements Initializable {
 
     @FXML
     private void exitProgramButton(ActionEvent event) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirm Exit");
+        alert.setHeaderText("Did you Save?");
+        alert.setContentText("Click to Exit");
     }
     
 }

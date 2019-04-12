f6390d17963d06c6101356046dffeca8d60cff52
Shad Nolen <shadnolen@msn.com>
2019-04-11 19:08:08 -0700

    Update MainScreenController.java

--- InvSupply/src/Controllers/MainScreenController.java
+++ InvSupply/src/Controllers/MainScreenController.java
@@ -12,6 +12,7 @@ import static Models.SupplyInv.getPartsOL;
 import static Models.SupplyInv.getProductsOL;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -23,6 +24,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
@@ -186,7 +188,10 @@ public class MainScreenController implements Initializable {
         alert.setTitle("Confirm Exit");
         alert.setHeaderText("Did you Save?");
         alert.setContentText("Click to Exit");
-     //  Option<Button>
+       Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == ButtonType.OK) {
+            System.exit(0);
+        }
     }
     
 }

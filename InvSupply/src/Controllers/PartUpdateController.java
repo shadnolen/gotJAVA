ff7338bd41e2daf015eac18498409cf8f27d05bd
Shad Nolen <shadnolen@msn.com>
2019-04-14 19:43:47 -0700

    Update PartUpdateController.java
    Working of the famed IF else

--- InvSupply/src/Controllers/PartUpdateController.java
+++ InvSupply/src/Controllers/PartUpdateController.java
@@ -5,15 +5,25 @@
  */
 package Controllers;
 
+import static Controllers.MainScreenController.selectedIndex;
+import Models.InHouse;
+import Models.OutSourced;
+import Models.SupplyInv;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -59,12 +69,64 @@ public class PartUpdateController implements Initializable {
     private void source(ActionEvent event) {
     }
 
-    @FXML
-    private void savePart(ActionEvent event) {
+   public Boolean isValid(){
+        return null;
+        
     }
+    @FXML
+    private void savePart(ActionEvent event) throws IOException{
+        if(isValid( this.partID.getText(),  this.partName.getText(), this.partCost.getText(), this.partMax.getText(),
+                this.partMin.getText(), this.partSupply.getText(), this.companyID.getText(), this.machineID.getText())){
+            if(this.toggle.getSelectedToggle().equals(.this.InHouse)){
+                   
+                    InHouse part = new InHouse();
+                    
+                    part.setMachID(Integer.parseInt(this.machineID.getText()));
+                    part.setPartID(Integer.parseInt(this.partID.getText()));
+                    part.setPartName(this.partName.getText());
+                    part.setPartPrice(Integer.parseInt(this.partCost.getText()));
+                    part.setPartMax(Integer.parseInt(this.partMax.getText()));
+                    part.setPartMin(Integer.parseInt(this.partMin.getText()));
+                    
+                    if(this.partID.getText().length()==0){
+                   
+                        part.setPartID(SupplyInv.getCountPart());                   
+                     }else{
+                        part.setPartID(Integer.parseInt(this.partID.getText()));
+                        SupplyInv.updatePart(selectedIndex(), part);
+                    }
+              }else{
+                   
+                    OutSourced part = new OutSourced();
+                    
+                    part.setCompanyName(Integer.parseInt(this.companyID.getText()));
+                    part.setPartID(Integer.parseInt(this.partID.getText()));
+                    part.setPartName(this.partName.getText());
+                    part.setPartPrice(Integer.parseInt(this.partCost.getText()));
+                    part.setPartMax(Integer.parseInt(this.partMax.getText()));
+                    part.setPartMin(Integer.parseInt(this.partMin.getText()));
+                    
+                    if(this.partID.getText().length()==0){
+                   
+                        part.setPartID(SupplyInv.getCountPart());                   
+                     }else{
+                        part.setPartID(Integer.parseInt(this.partID.getText()));
+                        SupplyInv.updatePart(selectedIndex(), part);
+                    }
+              }
+            
+        }
+    }
+    
+    
 
     @FXML
-    private void cancelPart(ActionEvent event) {
+    private void cancelPart(ActionEvent event) throws IOException {
+        Parent parent = FXMLLoader.load(getClass().getResource("/Views/MainScreen.fxml"));
+        Scene scene = new Scene(parent);
+        Stage stage = (Stage)((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.showAndWait();
     }
     
 }

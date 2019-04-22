cd2a8a4ba06aff7953a14dbdd16068449053ad5b
Shad Nolen <shadnolen@msn.com>
2019-04-22 09:32:13 -0700

    Update Descriptions.java

--- WashingtonTrail/src/washingtontrail/Descriptions.java
+++ WashingtonTrail/src/washingtontrail/Descriptions.java
@@ -11,4 +11,55 @@ package washingtontrail;
  */
 class Descriptions {
     
+    String leaderName, autoType, stateMap, music;
+    
+    
+    // Get the name of the leader    
+    public static String getLeaderName(){
+        return null;
+    }
+    
+    // Get auto name
+     public static String getAutoType(){
+        return null;
+    }
+     
+     //Get the map
+  public static String geStateMap(){
+        return null;
+    }  
+  
+  public static String getStateName(){
+      return null;
+  }
+
+//get the music in the tap deck
+ public static String getMusic(){
+        return null;
+    }
+ 
+ private static String[] state= {  "Minnesota", "North Dakota", "Montana", "Idaho", "Washington"};
+ 
+ public static String randomState(){
+         return null;
+ }
+ 
+ public static String randomCity(){
+     return null;
+ }
+ 
+ public static String healthEvent(){
+     return null;
+ }
+ 
+ public static String healthChange(){
+     return null;
+ }
+  
+ public static String coPilot(){
+     return null;
+ }
+ 
+ 
+    
 }
